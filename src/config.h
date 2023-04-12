0735a1d2ff5fb7769a2132bb3e4f973fcc3d6e27
Petr Prochazka <prochazkapetr2@gmail.com>
2023-04-12 18:48:34 +0200

    edited config

--- src/config.h
+++ src/config.h
@@ -1,21 +1,17 @@
-#include <WiFiMulti.h>
-#include <InfluxDbClient.h>
-#include <InfluxDbCloud.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <ArduinoJson.h>
+#include <Preferences.h>
+#include <PZEM004Tv30.h>
+#include "time.h"
 
-#define DEVICE "ESP32"
-// WiFi AP SSID
-#define WIFI_SSID "FILL HERE"
-// WiFi password
-#define WIFI_PASSWORD "FILL HERE"
-
-#define INFLUXDB_URL "FILL HERE"
-#define INFLUXDB_TOKEN "FILL HERE"
-#define INFLUXDB_ORG "FILL HERE"
-#define INFLUXDB_BUCKET "FILL HERE"
-
-// Time zone info
-#define TZ_INFO "UTC1"
-
-// NTP servers
-#define NTP1 "ntp.nic.cz"
-#define NTP2 "tik.cesnet.cz"
+// Configuration
+const char* ssid = "FILL HERE";
+const char* password = "FILL HERE";
+const char* mqtt_server = "FILL HERE"; // running on Home Assistant server
+const char* ntp_server = "tik.cesnet.cz";
+#define mqtt_user "FILL HERE"
+#define mqtt_password "FILL HERE"
+#define mqtt_id "esp32_client_test" //"esp32_client"
+#define mqtt_publish_topic "homeenergy/test" //"homeenergy/sensor1"
